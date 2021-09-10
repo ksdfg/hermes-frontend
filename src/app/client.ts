1483da64e7f1597027e7c538815da55017d8c1f5
ksdfg <ksdfg123@gmail.com>
2021-09-10 23:46:52 +0530

    feat: setup axios instance to use credentials
    So that we can work with the backend, which operates on cookies based sessions
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,9 +2,14 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND,
-  withCredentials: false,
+  withCredentials: true,
 });
 
-instance.interceptors.request.use(function (err) {
-  return Promise.reject(err);
-});
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (err) {
+    return Promise.reject(err);
+  }
+);
