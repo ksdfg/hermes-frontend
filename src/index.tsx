493f438de883ab64ae6518e94fb98f4bf9422358
ksdfg <ksdfg123@gmail.com>
2021-09-10 23:47:13 +0530

    feat: setup redux store provider in index
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -6,21 +6,29 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Homepage } from "./pages/Homepage/Homepage";
 import { Help } from "./pages/Help/Help";
+import { Qr } from "./pages/Qr/Qr";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <div className="flex flex-col w-full h-full">
-        <Switch>
-          <Route path="/help">
-            <Help />
-          </Route>
-          <Route path="/">
-            <Homepage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="flex flex-col w-full h-full">
+          <Switch>
+            <Route path="/help">
+              <Help />
+            </Route>
+            <Route path="/qr">
+              <Qr />
+            </Route>
+            <Route path="/">
+              <Homepage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
